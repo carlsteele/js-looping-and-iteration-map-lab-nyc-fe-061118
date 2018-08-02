7e099c2ce193b03d6ad6d1b049e320677903a31c
Carl Steele <casteeleu@gmail.com>
2018-08-02 00:50:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map((driver) => driver.toLowerCase()) 
+  
+}
+function createNameObject(driver) {
+  let nameArray = driver.split(" ")
+  let firstName = nameArray [0]
+  let lastName = nameArray[1]
+  return {firstName: firstName, lastName: lastName} 
+}
+function nameToAttributes(names) {
+  return names.map(createNameObject)  
+}
+function attributesToPhrase(drivers) {
+  let firstName =  
+}
+
+
+
+
+
